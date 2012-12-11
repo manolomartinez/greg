3d060e20e37ca1f7a21c146b06af3a5d0c372851
Manolo Martínez <manolo@austrohungaro.com>
2012-12-11 00:47:28 -0500

    Putting together package, executables and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(  name='Greg',
+        version='0.1.0',
+        description='A command-line podcast aggregator',
+        author='Manolo Martínez',
+        author_email='manolo@austrohungaro.com',
+        url='https://github.com/manolomartinez/greg',
+        packages=['greg'],
+        scripts=['bin/greg'],
+        data_files=[('/etc',['data/greg.conf'])]
+        )
