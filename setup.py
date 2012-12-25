94938260f248641e3415e64d9d3334968a2151d1
Manolo Martínez <manolo@austrohungaro.com>
2012-12-24 22:37:40 -0500

    corrected typo in setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(  name='Greg',
         url='https://github.com/manolomartinez/greg',
         packages=['greg'],
         scripts=['bin/greg'],
-        data_files=[('/etc',['data/greg.conf'])]
+        data_files=[('/etc',['data/greg.conf'])],
         license = 'GPLv3'
         )
