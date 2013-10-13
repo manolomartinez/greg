e1c94ad58226c8a133c4b2010900e213377fe9f9
Manolo Martínez <manolo@austrohungaro.com>
2013-10-13 06:41:43 -0400

    add feedparser dependence, and change version number

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(  name='Greg',
-        version='0.1.0',
+        version='0.4.0',
         description='A command-line podcast aggregator',
         author='Manolo Martínez',
         author_email='manolo@austrohungaro.com',
@@ -11,5 +11,6 @@ setup(  name='Greg',
         packages=['greg'],
         scripts=['bin/greg'],
         data_files=[('/etc',['data/greg.conf'])],
-        license = 'GPLv3'
+        license = 'GPLv3',
+        install_requires = ['feedparser']
         )
