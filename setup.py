cfa641d68aed9b809cd202cea78313ecb8f9daa6
Manolo Martínez <manolo@austrohungaro.com>
2015-06-15 17:54:52 +0200

    Cleaning setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-kwargs = {'name': 'Greg',
-          'version':  '0.4.4.3',
-          'install_requires': ['feedparser'],
-          'description': 'A command-line podcast aggregator',
-          'author': 'Manolo Martínez',
-          'author_email': 'manolo@austrohungaro.com',
-          'url': 'https://github.com/manolomartinez/greg',
-          'packages': ['greg'],
-          'entry_points': {'console_scripts': ['greg = greg.gregparser:main']},
-          'package_data': {'': ['data/greg.conf']},
-          'license': 'GPLv3'}
-
-setup(**kwargs)
+setup(
+    name='Greg',
+    version='0.4.4.4',
+    install_requires=['feedparser'],
+    description='A command-line podcast aggregator',
+    author='Manolo Martínez',
+    author_email='manolo@austrohungaro.com',
+    url='https://github.com/manolomartinez/greg',
+    packages=['greg'],
+    entry_points={'console_scripts': ['greg = greg.gregparser:main']},
+    package_data={'greg': ['data/*.conf']},
+    license='GPLv3'
+)
