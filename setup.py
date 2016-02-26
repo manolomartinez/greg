862e64b8f1c593b8d5037f5715086442f53b8544
Manolo Martínez <manolo@austrohungaro.com>
2016-02-26 10:24:13 +0100

    version 4.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Greg',
-    version='0.4.5.1',
+    version='0.4.6',
     install_requires=['feedparser'],
     description='A command-line podcast aggregator',
     author='Manolo Martínez',
